781a651511eaeb137af7fd44f0f24737cceea0df
rupabarma <108084091+rupabarma@users.noreply.github.com>
2023-03-14 20:01:36 +0530

    update index.js

--- index.js
+++ index.js
@@ -6,9 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import { Amplify, Auth } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
-
-Amplify.configure(awsconfig);
-
 AppRegistry.registerComponent(appName, () => App);
